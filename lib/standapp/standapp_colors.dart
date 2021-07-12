16b08631b2291fa39f6ca09143e4be053996d61c
Jascha Scholz <jascha_scholz@web.de>
2021-07-12 22:30:12 +0200

    adds new app colors

--- lib/standapp/standapp_colors.dart
+++ lib/standapp/standapp_colors.dart
@@ -11,4 +11,12 @@ class AppColors {
   static const Color gray = Color.fromRGBO(249, 249, 249, 1);
   static const Color button_color = Color.fromRGBO(232, 232, 232, 1);
   static const Color button_text_disable = Color.fromRGBO(102, 102, 102, 1);
+
+  static const Color jascha_red = const Color(0xFFFF685F);
+  static const Color baby_blue = const Color(0xFFE0F3F8);
+  static const Color standard_blue = const Color(0xFF263238);
+  static const Color audi_grey = const Color(0xFF666666);
+  static const Color dark_grey = const Color(0xFF979797);
+  static const Color fifty_shades = const Color(0xFFE8E8E8);
+  static const Color weisser_als_weiss = const Color(0xFFFFFFFF);
 }
