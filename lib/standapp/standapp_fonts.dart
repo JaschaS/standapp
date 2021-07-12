f0d2acdcb5333d5c3e8c23392fce58c98e2987a4
Jascha Scholz <jascha_scholz@web.de>
2021-07-12 22:44:33 +0200

    adds new app font sizes

---
+++ lib/standapp/standapp_fonts.dart
@@ -0,0 +1,9 @@
+class AppFonts {
+  AppFonts._();
+
+  static const double h1 = 40;
+  static const double h2 = 32;
+  static const double h3 = 24;
+  static const double h4 = 20;
+  static const double h5 = 16;
+}
