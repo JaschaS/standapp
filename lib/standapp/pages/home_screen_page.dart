1f191890636a89c12ee6040580aad09b551bf51e
Ivan Lazarev <ivan.lazarev@audi.de>
2021-07-23 15:17:47 +0200

    renaming header widget to home screen header

--- lib/standapp/pages/home_screen_page.dart
+++ lib/standapp/pages/home_screen_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:standapp/standapp/host_screen/background_widget.dart';
 import 'package:standapp/standapp/widgets/home_screen/email_signin.dart';
-import 'package:standapp/standapp/widgets/home_screen/header_widget.dart';
+import 'package:standapp/standapp/widgets/home_screen/home_screen_header.dart';
 
 class HomeScreenPage extends StatefulWidget {
   @override
