3fc35934407e6f7ab329912c1813640465e57578
Jascha Scholz <jascha_scholz@web.de>
2021-08-08 17:09:49 +0200

    updates width of date button to fix overflow

--- lib/standapp/widgets/host_screen/avatar_tile_widget.dart
+++ lib/standapp/widgets/host_screen/avatar_tile_widget.dart
@@ -75,7 +75,7 @@ class AvatarTile extends StatelessWidget {
                 height: 180,
               ),
               Container(
-                width: 495,
+                width: 560,
                 height: 69,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
@@ -140,7 +140,7 @@ class _DateButton extends StatelessWidget {
   @override
   Widget build(final BuildContext context) {
     return SizedBox(
-      width: 190,
+      width: 225,
       height: 40,
       child: ElevatedButton.icon(
         style: ButtonStyle(
