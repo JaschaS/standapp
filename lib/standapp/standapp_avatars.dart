19ac4f5f2a7d3bc5aae2be12303bcc6a1efcd4a7
Jascha Scholz <jascha_scholz@web.de>
2021-08-24 19:04:11 +0200

    removing gray-avatar from standapp-avatars

--- lib/standapp/standapp_avatars.dart
+++ lib/standapp/standapp_avatars.dart
@@ -11,24 +11,11 @@ class AvatarsImages {
       if (i != 0 && i != 2) "images/Avatar${i < 10 ? "0$i" : i}.png"
   ];
 
-  static List<String> imagesBlack = [
-    for (var i = 0; i < 21; i += 1)
-      if (i != 0 && i != 2) "images/Avatar${i < 10 ? "0$i" : i}-1.png"
-  ];
-
   static String randomAvatar() {
     final random = Random();
 
     final index = random.nextInt(imagesColored.length);
 
-    return random.nextBool() ? imagesColored[index] : imagesBlack[index];
-  }
-
-  static String randomBlackAvatar() {
-    final random = Random();
-
-    final index = random.nextInt(imagesBlack.length);
-
-    return imagesBlack[index];
+    return imagesColored[index];
   }
 }
