be089f5bf4fd71275360b63f9ea602abe5f5a57d
Jascha Scholz <jascha_scholz@web.de>
2021-08-24 12:25:51 +0200

    adds a test main for widget experiments

---
+++ lib/testapp.dart
@@ -0,0 +1,165 @@
+import 'package:flutter/material.dart';
+import 'package:standapp/standapp/standapp_buttons.dart';
+import 'package:standapp/standapp/standapp_colors.dart';
+import 'package:standapp/standapp/standapp_fonts.dart';
+import 'package:standapp/standapp/widgets/host_screen/avatar_tile_widget.dart';
+import 'package:standapp/standapp/widgets/host_screen/select_host_widget.dart';
+
+void main() {
+  runApp(TestApp());
+}
+
+class TestApp extends StatelessWidget {
+  @override
+  Widget build(final BuildContext context) {
+    return MaterialApp(
+      title: "StandUp-Host",
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        body: Center(
+          child: AvatarTile(
+            title: _header(),
+            description: Text(
+              "I am your next host",
+              style: AppFonts.textStyleWithSize(
+                AppFonts.h5,
+                weight: FontWeight.bold,
+              ),
+            ),
+            primary: PrimaryAppButton(
+              title: "Confirm host",
+              callback: () {},
+            ),
+            secondary: SecondaryAppButton(
+              title: "Search again",
+              callback: () {},
+            ),
+            alternate: TextAppButton(
+              callback: () {},
+              title: "Cancel",
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _header() {
+    return Text(
+      "Hi, I am Jascha",
+      style: AppFonts.textStyleWithSize(
+        AppFonts.h2,
+        weight: FontWeight.bold,
+        color: AppColors.standard_blue,
+      ),
+    );
+  }
+}
+
+class Calendar extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => CalendarState();
+}
+
+class CalendarState extends State<Calendar> {
+  DateTime _currentDate = DateTime.now();
+
+  @override
+  Widget build(final BuildContext context) {
+    return Center(
+      child: Container(
+        width: 460,
+        height: 250,
+        color: Colors.red,
+        child: Column(
+          children: [
+            _header(this._currentDate),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _header(final DateTime date) {
+    return Row(
+      children: [
+        Text(
+          "${_month(date)} ${date.year}",
+          style: AppFonts.textStyleWithSize(
+            AppFonts.h3,
+          ),
+        ),
+        const Spacer(),
+        TextButton(
+          onPressed: _onPreviousMonth,
+          child: Icon(Icons.chevron_left),
+        ),
+        TextButton(
+          onPressed: _onToday,
+          child: Text("Today"),
+        ),
+        TextButton(
+          onPressed: _onNextMonth,
+          child: Icon(Icons.chevron_right),
+        )
+      ],
+    );
+  }
+
+  void _onToday() {
+    setState(() {
+      this._currentDate = DateTime.now();
+    });
+  }
+
+  void _onPreviousMonth() {
+    setState(() {
+      this._currentDate = DateTime(
+        this._currentDate.year,
+        this._currentDate.month - 1,
+        this._currentDate.day,
+      );
+    });
+  }
+
+  void _onNextMonth() {
+    setState(() {
+      this._currentDate = DateTime(
+        this._currentDate.year,
+        this._currentDate.month + 1,
+        this._currentDate.day,
+      );
+    });
+  }
+
+  String _month(final DateTime date) {
+    switch (date.month) {
+      case DateTime.january:
+        return "January";
+      case DateTime.february:
+        return "February";
+      case DateTime.march:
+        return "March";
+      case DateTime.april:
+        return "April";
+      case DateTime.may:
+        return "May";
+      case DateTime.june:
+        return "June";
+      case DateTime.july:
+        return "July";
+      case DateTime.august:
+        return "August";
+      case DateTime.september:
+        return "September";
+      case DateTime.october:
+        return "October";
+      case DateTime.november:
+        return "November";
+      case DateTime.december:
+        return "December";
+      default:
+        return "ERROR";
+    }
+  }
+}
